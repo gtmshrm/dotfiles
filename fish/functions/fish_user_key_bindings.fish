8a37f78f16ec7c0f3231243a89f48028a2a8e7b2
Gautam Sharma <gautamsharma7234@gmail.com>
2022-07-31 23:46:40 -0400

    Add fzf support for fish

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -2,4 +2,5 @@ function fish_user_key_bindings
   fish_vi_key_bindings
   bind -M insert -m default jk backward-char force-repaint
   bind -M insert \cn accept-autosuggestion
+  fzf_key_bindings
 end
